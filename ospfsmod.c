59a02ca589606d9b626a31814eb3517fda10954d
James Danylik <james.danylik@gmail.com>
2013-11-14 22:48:58 -0800

    Added names/uids to ospfsmod and answers.

--- ospfsmod.c
+++ ospfsmod.c
@@ -1228,6 +1228,6 @@ module_init(init_ospfs_fs)
 module_exit(exit_ospfs_fs)
 
 // Information about the module
-MODULE_AUTHOR("Skeletor");
+MODULE_AUTHOR("James Danylik & Zhaoying Yao");
 MODULE_DESCRIPTION("OSPFS");
 MODULE_LICENSE("GPL");
